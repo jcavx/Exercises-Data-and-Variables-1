6d6016316ea858f62d904dbc10b8ee636d8126f4
Julio Alarcon <julio.alarcon@icloud.com>
2021-05-24 04:44:20 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// 1. Declare and assign the variables here:
+let shuttleName = 'Determination';
+let shuttleSpeed = '17500';
+let distMars = 225000000;
+let distMoon = 384400;
+const millePerKm = .621;
+
+
+// 2. Use console.log to print the 'typeof' each variable. Print one item per line.
+console.log(typeof(shuttleName));
+console.log(typeof(shuttleSpeed));
+console.log(typeof(distMars));
+console.log(typeof(distMoon));
+console.log(typeof(millePerKm));
+// Code your solution to exercises 3 and 4 here:
+
+
+
+
+
+
+
+
+// Code your solution to exercise 5 here:
